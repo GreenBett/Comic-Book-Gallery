3aa52c951e17dad22e6f25920b451ed40586bd5f
GreenBett <green95551@protonmail.com>
2017-12-28 14:26:58 -0600

    Refactored Detail Action Method to use Action Control Types

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -4,16 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+/*
+ - Notes: Pattern = controller/action
+ -        Controller = ComicBooks, Action = Detail
+ -        Using URL Routing
+ */
 namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller //Controllers --Must be public for access
     {
 
-        public string Detail()
+        public ActionResult Detail()
         { //Action Method  --Must be public for access
 
-            return "Hello from the comic books controller!";
-        }
 
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Thursday)
+            {
+
+                return Redirect("/");
+            }
+            return Content("Hello from the comic books controller!");
+
+         
+        // Content = "Hello from the comic books controller!"  //Action Result Types
     }
+
+}
 }
