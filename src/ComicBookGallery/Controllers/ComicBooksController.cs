69861eca97edebb6d8fe97bb9ac25bb31128f1b7
GreenBett <green95551@protonmail.com>
2017-12-29 08:25:50 -0600

    changed from Vars inside of the View like inline styles. changed those into a ViewBag Property

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -16,7 +16,20 @@ namespace ComicBookGallery.Controllers
 
         public ActionResult Detail()
         { //Action Method  --Must be public for access
-     
+
+            //dynamic type changed from var seriesTitle to a property
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
 
             return View();
         }
