162a6b29f87f2dc038b5b10f79a25a31c989770e
GreenBett <green95551@protonmail.com>
2017-12-29 09:09:48 -0600

    Updated ComicBook Model

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -15,6 +15,21 @@ namespace ComicBookGallery.Models
         public Artists[] Artists { get; set; }
         public bool Favorite { get; set; }
 
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
 
+        //series-title-issuenumber.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }
