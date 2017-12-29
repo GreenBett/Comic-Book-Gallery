4cf80e9863e6acbacf160396b009fdcc1fa246df
GreenBett <green95551@protonmail.com>
2017-12-29 09:05:16 -0600

    Added ComicBook and Artist Models

---
+++ src/ComicBookGallery/Models/Artists.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class Artists
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+
+    }
+}
---
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artists[] Artists { get; set; }
+        public bool Favorite { get; set; }
+
+
+    }
+}
