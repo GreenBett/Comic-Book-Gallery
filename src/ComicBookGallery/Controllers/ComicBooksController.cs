730b0631a0323fc1800195e719204f1a9f2d7013
GreenBett <green95551@protonmail.com>
2018-01-01 22:17:08 -0600

    Added ComicBookRepo

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,9 +1,10 @@
-﻿using ComicBookGallery.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
+using ComicBookGallery.Models;
 
 /*
  - Notes: Pattern = controller/action
@@ -18,21 +19,7 @@ namespace ComicBookGallery.Controllers
         public ActionResult Detail()
         { //Action Method  --Must be public for access
 
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artists[]
-                {
-                    new Artists() { Name = "Dan Slott", Role = "Script"},
-                    new Artists() { Name = "Humberto Ramos", Role = "Pencils"},
-                    new Artists() { Name = "Victor Olazaba", Role = "Inks"},
-                    new Artists() { Name = "Edgar Delgado", Role = "Colors"},
-                    new Artists() { Name = "Chris Eliopoulos", Role = "Letters"}
-                }
-
-            };             
+                
 
             return View(comicBook);
         }
---
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using ComicBookGallery.Models;
+
+namespace ComicBookGallery.Data
+{
+    public class ComicBookRepository
+    {
+
+        private static ComicBook[] _comicBooks = new ComicBook[]
+        {
+            new ComicBook()
+            {
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>",
+                Artists = new Artists[]
+                {
+                    new Artists() { Name = "Dan Slott", Role = "Script" },
+                    new Artists() { Name = "Humberto Ramos", Role = "Pencils" },
+                    new Artists() { Name = "Victor Olazaba", Role = "Inks" },
+                    new Artists() { Name = "Edgar Delgado", Role = "Colors" },
+                    new Artists() { Name = "Chris Eliopoulos", Role = "Letters" },
+                },
+                Favorite = false
+            },
+            new ComicBook()
+            {
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 657,
+                DescriptionHtml = "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
+                Artists = new Artists[]
+                {
+                    new Artists() { Name = "Dan Slott", Role = "Script" },
+                    new Artists() { Name = "Marcos Martin", Role = "Pencils" },
+                    new Artists() { Name = "Marcos Martin", Role = "Inks" },
+                    new Artists() { Name = "Muntsa Vicente", Role = "Colors" },
+                    new Artists() { Name = "Joe Caramagna", Role = "Letters" }
+                },
+                Favorite = false
+            },
+            new ComicBook()
+            {
+                SeriesTitle = "Bone",
+                IssueNumber = 50,
+                DescriptionHtml = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
+                Artists = new Artists[]
+                {
+                    new Artists() { Name = "Jeff Smith", Role = "Script" },
+                    new Artists() { Name = "Jeff Smith", Role = "Pencils" },
+                    new Artists() { Name = "Jeff Smith", Role = "Inks" },
+                    new Artists() { Name = "Jeff Smith", Role = "Letters" }
+                },
+                Favorite = false
+            }
+        };
+
+        public ComicBook GetComicBook(int id)
+        {
+            ComicBook comicBookToReturn = null;
+
+            foreach (var comicBook in _comicBooks)
+            {
+                if (comicBook.Id == id)
+                {
+                    comicBookToReturn = comicBook;
+
+                    break;
+                }
+            }
+
+            return comicBookToReturn;
+        }
+
+    }
+}
